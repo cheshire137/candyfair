69ea59cdd1a38df2c8aed1ddca21c851d2b7801e
Sarah Vessels <cheshire137@gmail.com>
2017-08-09 23:34:49 -0500

    Drop trackable module from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :omniauthable,
-  # :recoverable, :rememberable, :validatable
-  devise :database_authenticatable, :trackable, :registerable,
+  # :recoverable, :rememberable, :validatable, :trackable
+  devise :database_authenticatable, :registerable,
          authentication_keys: [:username]
 
   validates :username, presence: true, uniqueness: {case_sensitive: false}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031021001) do
+ActiveRecord::Schema.define(version: 20170810043226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,11 +66,6 @@ ActiveRecord::Schema.define(version: 20151031021001) do
   create_table "users", force: :cascade do |t|
     t.string   "username",           default: "", null: false
     t.string   "encrypted_password", default: "", null: false
-    t.integer  "sign_in_count",      default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
   end
