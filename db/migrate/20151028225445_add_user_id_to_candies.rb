e614eaebd9d305823a398560db44d390b4b4d074
Sarah Vessels <cheshire137@gmail.com>
2017-08-09 23:34:55 -0500

    Fix migration when no users

--- db/migrate/20151028225445_add_user_id_to_candies.rb
+++ db/migrate/20151028225445_add_user_id_to_candies.rb
@@ -1,8 +1,9 @@
 class AddUserIdToCandies < ActiveRecord::Migration
   def up
     add_column :candies, :user_id, :integer
-    user = User.first
-    execute "UPDATE candies SET user_id = #{user.id}"
+    if user = User.first
+      execute "UPDATE candies SET user_id = #{user.id}"
+    end
     add_index :candies, [:user_id, :name], unique: true
     change_column_null :candies, :user_id, false
   end
